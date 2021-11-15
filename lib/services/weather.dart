9a188ceb63ccac85ca4bc36a9723184cf01236a7
André Luís Martins de Oliveira <almoliveira.info@gmail.com>
2021-11-15 14:17:03 -0300

    Finished

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,34 @@
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+
+import '../keys.dart';
+
+const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
+
 class WeatherModel {
+  Future<dynamic> getCityWeather(String cityName) async {
+    String apiKey = APIKey.getApiKey();
+
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    String apiKey = APIKey.getApiKey();
+
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
@@ -31,3 +61,7 @@ class WeatherModel {
     }
   }
 }
+// String apiKey = APIKey.getApiKey();
+//
+// NetworkHelper networkHelper = NetworkHelper(
+//     '$WEATHER_URL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey');
